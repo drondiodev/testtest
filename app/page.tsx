30ec8076e9d14b73c066bdd92521ea2afe59f7ed
David Rondio <103181452+drondiodev@users.noreply.github.com>
2024-09-17 16:31:29 +0200

    WIP: modify page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,7 @@
-import Link from 'next/link';
-
 export default function Home() {
   return (
     <section className="py-14">
-      <div className="flex flex-col justify-center items-center mx-auto px-4 md:px-8">
-        <BlockQuote
-          quote="Template de configuration d'un projet Next.Js avec Typescript, ESLint, Prettier, Husky, Docker, scopes de commit personnalisés et Lint Staged."
-          author="Drondiodev"
-        />
-        <Link
-          className="flex items-center rounded-md bg-red-500/15 mt-5 px-2 py-0.5 gap-3 text-red-500 text-sm"
-          href="https://github.com/drondiodev/nextjs14cicd"
-          target="_blank"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="lucide lucide-sparkles "
-          >
-            <path d="m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z" />
-            <path d="M5 3v4" />
-            <path d="M19 17v4" />
-            <path d="M3 5h4" />
-            <path d="M17 19h4" />
-          </svg>
-          <span className="truncate">Donner une étoile au répo </span>
-        </Link>
-      </div>
+      <div>hello world</div>
     </section>
   );
 }
-
-const BlockQuote = ({ quote, author }: { quote: string; author: string }) => {
-  return (
-    <blockquote className="bg-gray-500/15 text-gray-700 dark:bg-gray-500/10 dark:text-blue-400 border-l-4 border-r-4 border-blue-500/70 py-2 px-4 rounded-xl">
-      <p className="inline italic">{quote}</p>
-      <p className="text-sm text-end tracking-tighter italic font-semibold mt-1.5">
-        {author}
-      </p>
-    </blockquote>
-  );
-};
